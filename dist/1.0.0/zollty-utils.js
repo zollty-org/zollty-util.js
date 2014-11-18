5af268fada41ca08092fbc611a5f4de4e5720a5d
zollty <zollty@163.com>
2014-11-18 17:50:47 +0800

    add version

--- dist/1.0.0/zollty-utils.js
+++ dist/1.0.0/zollty-utils.js
@@ -1,4 +1,4 @@
-﻿/*!zollty-util.js - http://zollty-org.github.io/zollty-util.js/*/
+﻿/*!zollty-util.js 1.0.0 - http://zollty-org.github.io/zollty-util.js/*/
 !function(t,e){"object"==typeof module&&"object"==typeof module.exports?module.exports=t.document?e(t,!0):function(t){if(!t.document)throw Error("zt requires a window with a document")
 return e(t)}:e(t)}("undefined"!=typeof window?window:this,function(t,e){function n(){return void 0!==t.ActiveXObject?new t.ActiveXObject("Microsoft.XMLHTTP"):new t.XMLHttpRequest}function r(){return a.baseURL?a.baseURL:(a.baseURL=u(),a.baseURL)}function o(){var t
 try{t=location.href}catch(e){t=document.createElement("a"),t.href="",t=t.href}return t}function u(){var e=o(),n=e.indexOf("?");-1!=n&&(e=e.substring(0,n))
